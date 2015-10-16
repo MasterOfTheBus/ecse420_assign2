3e736460ddb31ad2f740b7f7085e3c43d5a65b81
Sidney Ng <sidney.ng@mail.mcgill.ca>
2015-10-15 23:33:27 -0400

    reading the file in parallel

--- cp_mpi.c
+++ cp_mpi.c
@@ -3,6 +3,8 @@
 #include <mpi.h>
 
 void get_rows_cols(char *filename, int* cols_rows);
+double** allocate_matrix(int rowss, int columns);
+void fill_matrix_portion(char *filename, double **matrix, int start_row, int end_row, int columns);
 
 int main(int argc, char* argv[]) {
 
@@ -12,7 +14,8 @@ int main(int argc, char* argv[]) {
   }
 
   int size, rank;
-  int columns, rows; 
+  int columns, rows;
+  double **matrix; 
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -20,17 +23,21 @@ int main(int argc, char* argv[]) {
   if (rank == 0) {
     int cols_rows[2];
     get_rows_cols(argv[1], cols_rows);
-    printf("columns: %d, rows: %d\n", cols_rows[0], cols_rows[1]);
     columns = cols_rows[0];
     rows = cols_rows[1];
   }
 
+  // broadcast the column and row info
   MPI_Bcast(&columns, 1, MPI_INT, 0, MPI_COMM_WORLD);
   MPI_Bcast(&rows, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-  if (rank != 0) {
-    printf("columns: %d, rows: %d\n", columns, rows);
-  }
+  // determine the portion of the matrix to take
+  int start_row = rows / size * rank;
+  int end_row = rows / size * (rank + 1); 
+  if (rank == size - 1) end_row += rows % size;
+
+  matrix = allocate_matrix(end_row - start_row, columns);
+  fill_matrix_portion(argv[1], matrix, start_row, end_row, columns);
 
   MPI_Finalize();
 
@@ -59,11 +66,46 @@ void get_rows_cols(char *filename, int* cols_rows) {
       }
     }
 
-/*
-    // determine the portion of the matrix to take
-    int start_row = *rows / size * rank;
-    int end_row = *rows / size * (rank + 1); 
-    if (rank == size - 1) end_row += *rows % size;
-*/
+    fclose(file);
+
     return;
+}
+
+double ** allocate_matrix(int rows, int cols)
+{
+  int i = 0;
+  double ** matrix = (double **) malloc(rows * sizeof(double *));
+
+  for (i = 0; i < rows; i++) {
+    matrix[i] = (double *) malloc(cols * sizeof(double));
+  }
+
+  return matrix;
+}
+
+void fill_matrix_portion(char *filename, double **matrix, int start_row, int end_row, int columns) {
+  FILE *file;
+  file = fopen(filename, "r");
+
+  // position the file pointer first to the right row
+  int i = 0;
+  char c;
+  while (i != start_row) {
+    c = fgetc(file);
+    if (c == '\n') i++;
+  }
+
+  // read in the data
+  i = 0;
+  int rows = end_row - start_row;
+  for (; i < rows; i++) {
+    int j = 0;
+    for (; j < columns; j++) {
+      fscanf(file, "%lf", &matrix[i][j]);
+    }
+  }
+
+  fclose(file);
+
+  return;
 }
